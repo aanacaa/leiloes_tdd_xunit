a220b89706fbee06cbf44b853a9b1e8a81eb657e
Ana Andrade <anacrln.andrade@gmail.com>
2020-12-28 21:29:11 -0300

    feat: Testes de unidade e TDD com xUnit

---
+++ Alura.LeilaoOnline.Core/Leilao.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Alura.LeilaoOnline.Core
+{
+
+    public enum EstadoLeilao
+    {
+        LeilaoAntesDoPregao,
+        LeilaoEmAndamento,
+        LeilaoFinalizado
+    }
+
+    public class Leilao
+    {
+
+        public IEnumerable<Lance> Lances => _lances;
+        public string Peca { get; }
+        public Lance Ganhador { get; private set; }
+        public EstadoLeilao Estado { get; private set; }
+        public double ValorDestino { get; }
+        private IModalidadeAvaliacao _avaliador;
+        private IList<Lance> _lances;
+        private Interessada _ultimoCliente;
+
+        public Leilao(string peca, IModalidadeAvaliacao avaliador)
+        {
+            Peca = peca;
+            _lances = new List<Lance>();
+            Estado = EstadoLeilao.LeilaoAntesDoPregao;
+            _avaliador = avaliador;
+
+
+        }
+        private bool NovoLanceEhAceito(Interessada cliente, double valor)
+        {
+            return (Estado == EstadoLeilao.LeilaoEmAndamento)
+                && (cliente != _ultimoCliente);
+        }
+
+
+        public void RecebeLance(Interessada cliente, double valor)
+        {
+            if (NovoLanceEhAceito(cliente, valor))
+            {
+
+                _lances.Add(new Lance(cliente, valor));
+                _ultimoCliente = cliente;
+            }
+        }
+
+        public void IniciaPregao()
+        {
+            Estado = EstadoLeilao.LeilaoEmAndamento;
+
+        }
+
+        public void TerminaPregao()
+        {
+            if (Estado != EstadoLeilao.LeilaoEmAndamento)
+            {
+                throw new System.InvalidOperationException("Não é possível terminar o pregão sem que ele tenha começado. Para isso, utilize o método IniciaPregao().");
+            }
+            Ganhador = _avaliador.Avalia(this);
+            Estado = EstadoLeilao.LeilaoFinalizado;
+        }
+    }
+}
